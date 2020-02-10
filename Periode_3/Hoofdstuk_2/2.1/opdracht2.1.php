ac92f367850b83d13d9cdc36b1d3d5d0d07df1ce
Mathijs Nabbe <mathijs.nabbe@edu-kw1c.nl>
2020-02-10 13:42:47 +0100

    Opdracht 2.1 commit

---
+++ Periode_3/Hoofdstuk_2/2.1/opdracht2.1.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * User: Mathijs Nabbe
+ * Date: 10-02-2020
+ * Time: 13:34 AM
+ * File: opdracht2.1.php
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <title><?php print "opdracht 2.1"; ?></title>
+        <link href="css/style.css">
+    </head>
+    <body>
+        <p><?php echo "Hello World!" ?></p>
+    </body>
+</html>
